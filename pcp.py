7b4710ebe418967a9097777fe37f43bc580b0fd1
Nicolas JEANNE <jeanne.n@chu-toulouse.fr>
2022-07-19 10:51:45 +0200

    modified plot title and log file

--- pcp.py
+++ pcp.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     Distributed on an "AS IS" basis without warranties or conditions of any kind, either express or implied.
 
     Produce a plot of the contribution of the positions of a sequence. 
-    The contribution of the position is computed using 
+    The contributions are 
     A threshold is set to select only the positions above it. 
     """
     parser = argparse.ArgumentParser(description=descr, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -90,7 +90,6 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--position-col", required=True, type=str, help="the name of the position column.")
     parser.add_argument("-x", "--target-col", required=True, type=str,
                         help="the name of the target contribution column.")
-    parser.add_argument("-z", "--total-col", required=True, type=str, help="the name of the total contribution column.")
     parser.add_argument("-s", "--sequence-size", required=False, type=int, help="the size of the studied sequence.")
     parser.add_argument("-l", "--log", required=False, type=str,
                         help="the path for the log file. If this option is skipped, the log file is created in the "
@@ -128,10 +127,17 @@ if __name__ == "__main__":
         seq_size = args.sequence_size
     else:
         seq_size = int(df[args.position_col].max())
-
-    features = [GraphicFeature(start=0, end=seq_size, strand=+1, color="#ffd700", label="Spike")]
+    logging.info(f"sequence size ({'provided by the user' if args.sequence_size else 'last position of contribution'}):"
+                 f" {seq_size}")
+    features = [GraphicFeature(start=0, end=seq_size, strand=+1, color="#ffd700", label="Spike",
+                               fontdict={"weight": "bold"})]
+    nb_pass_contrib = 0
+    nb_contrib = 0
     for index, row in df.iterrows():
+        if row[args.target_col] > 0:
+            nb_contrib += 1
         if row[args.target_col] >= args.threshold:
+            nb_pass_contrib += 1
             color = None
             for local_threshold in sorted(hue.keys()):
                 color = hue[local_threshold]
@@ -144,8 +150,12 @@ if __name__ == "__main__":
                                            color=color,
                                            box_color=color,
                                            label=f"{int(row[args.position_col])}: {round(row[args.target_col], 2)}%"))
+    prop_passed = nb_pass_contrib / nb_contrib
+    logging.info(f"{nb_pass_contrib}/{nb_contrib} positions with a contribution >= {args.threshold}% contribution "
+                 f"threshold ({prop_passed:.2%})")
 
     record = GraphicRecord(sequence_length=seq_size, features=features, plots_indexing="genbank")
     ax, _ = record.plot(draw_line=True, figure_width=20)
-    ax.set_title(f"Positions contribution: {args.target_col.replace('_', ' ')}", loc="left", weight="bold")
+    ax.set_title(f"Contributions {args.target_col.replace('_', ' ')}: {prop_passed:.2%} of {nb_contrib} positions >= "
+                 f"{args.threshold}% contribution threshold", loc="left", weight="bold")
     ax.figure.savefig(args.out, bbox_inches="tight")
